a97278881b636516707464f1c4cc707a1e86ed4a
andrew-cheng <andrew.cheng@gmail.com>
2023-10-13 19:52:46 -0500

    set up basic table

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,47 @@
 import "./style.css";
 
+const tableData: string[][] = [];
+new Array(1000).fill(0).forEach((_) => {
+  const row: string[] = [];
+  new Array(9).fill(0).forEach((_) => {
+    row.push(
+      (Math.random() * 100).toLocaleString("en", {
+        maximumFractionDigits: 2,
+        useGrouping: false,
+      })
+    );
+  });
+  tableData.push(row);
+});
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>col 1</th>
+        <th>col 2</th>
+        <th>col 3</th>
+        <th>col 4</th>
+        <th>col 5</th>
+        <th>col 6</th>
+        <th>col 7</th>
+        <th>col 8</th>
+        <th>col 9</th>
+      </tr>
+    </thead>
+    <tbody>
+      
+    </tbody>
+  </table>
 `;
+
+const tbody = document.querySelector("tbody");
+tableData.forEach((row) => {
+  const tr = document.createElement("tr");
+  row.forEach((cell) => {
+    const td = document.createElement("td");
+    td.innerHTML = cell;
+    tr.appendChild(td);
+  });
+  tbody?.appendChild(tr);
+});
